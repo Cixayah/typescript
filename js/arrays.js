a02e7b419d49db09ab39f4e916292c1446edf5b4
Cix <gabrielviniciodacosta@gmail.com>
2024-11-05 14:19:12 -0300

    delete js

--- js/arrays.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const numbersArray = [1, 2, 3, 4, 5];
-console.log('Array de números:', numbersArray);
-console.log('Primeiro elemento:', numbersArray[0]);
-console.log('Último elemento:', numbersArray[numbersArray.length - 1]);
--- js/for.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
-}
-const nameC = 'Gabriel';
-for (let y = 1; y <= 3; y++) {
-    console.log(nameC, 'x', y);
-}
